ef0fb49db59f941e41f3c5cc6b37fc4add1f52e1
iJuanma26 <jmcepeda97@gmail.com>
2022-08-19 22:50:58 -0500

    Se introduce un print del commit de Juan Cepeda

--- Ejemplo.java
+++ Ejemplo.java
@@ -4,6 +4,8 @@ package com.colombiansoftwareengineers.ejemplo;
 public class Ejemplo {
 
     public static void main(String[] args) {
+
         System.out.println("Hola GitHub!");
+        System.out.print("Este es el commit de Juan Cepeda");
     }
 }
